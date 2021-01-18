53bcf4650051495dd0da110c6ccc8e3b95dc8765
jordialvarez22 <jalvarez@cifpfbmoll.eu>
2021-01-18 17:40:30 +0100

    US13 T1 Have a menu in the top of the web application to access the differents options.

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const router = express.Router()
 //desc Login/Landin page
 //@route get /
 router.get('/', (req, res) => {
-    res.render('login')
+    res.render('home')
 })
 
 //desc Dashboard
 //@route get /dashboard
-router.get('/dashboard', (req, res) => {
+/*router.get('/dashboard', (req, res) => {
     res.render('dashboard')
-})
+})*/
 
 module.exports = router
--- server.js
+++ server.js
@@ -4,15 +4,14 @@ const path = require('path');
 
 const app = express();
 
-// Static folder
 app.use(express.static(path.join(__dirname, 'public')))
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+});*/
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
 
 // Handlebars
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}));
@@ -20,3 +19,5 @@ app.set('view engine', '.hbs');
 
 //Routes
 app.use('/', require('./routes/index'))
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
