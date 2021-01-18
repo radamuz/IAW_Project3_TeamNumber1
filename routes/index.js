2bd5b3cfb5d2d30341eab78abc2badca4787827e
jordialvarez22 <jalvarez@cifpfbmoll.eu>
2021-01-18 16:02:44 +0100

    Sidenav

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+//desc Login/Landin page
+//@route get /
+router.get('/', (req, res) => {
+    res.render('login')
+})
+
+//desc Dashboard
+//@route get /dashboard
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard')
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const exphbs = require('express-handlebars')
 const path = require('path');
 
 const app = express();
 
+// Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -11,3 +13,10 @@ app.get('/', function(req, res) {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// Handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+//Routes
+app.use('/', require('./routes/index'))
