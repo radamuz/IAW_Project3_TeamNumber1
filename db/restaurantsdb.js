eb0f6b1076c2040110cd7ce0461a0f4ef5080242
jordialvarez22 <jalvarez@cifpfbmoll.eu>
2021-02-09 12:31:40 +0100

    DB Updated

--- db/restaurantsdb.js
+++ db/restaurantsdb.js
@@ -2,6 +2,8 @@ db.restaurants.insertMany([{
         name: 'Hostelería Dromedarios',
         forks: '5',
         img: 'img/dromedario-rest.jpg',
+        phone: '658452399',
+        price: '€€€',
         foods: ['ensalada cesar', 'spaguetis carbonara', 'pescaíto frito', 'kebab', 'tostadas'],
         services: ['baños', 'bolos'],
         category: 'hostelry',
@@ -17,12 +19,21 @@ db.restaurants.insertMany([{
             comment: 'Gran restaurante, mejores kebabs',
             stars: 5,
             date: Date()
-        }]
+        },
+        {
+            author: 'David Broncano',
+            comment: 'Me flipan los Kebabs estos moros son la ostia',
+            stars: 4,
+            date: 'Fri Feb 05 2021 23:30:26 GMT+0000 (UTC)'
+        }
+    ]
     },
     {
         name: 'La Casa del Goloso',
         forks: '4',
         img: 'img/goloso.jpg',
+        phone: '658411355',
+        price: '€€',
         foods: ['Creppes', 'Gofres', 'Tartas', 'Donetes', 'tostadas'],
         services: ['baños'],
         category: 'hostelry',
@@ -38,12 +49,21 @@ db.restaurants.insertMany([{
             comment: 'El mejor restaurante para comidas poco saludables',
             stars: 5,
             date: Date()
-        }]
+        },
+        {
+            author: 'Lucía La Piedra',
+            comment: 'El mejor restaurante para comidas poco saludables',
+            stars: 2,
+            date: 'Fri Feb 05 2021 23:30:26 GMT+0000 (UTC)'
+        }
+    ]
     },
     {
         name: 'La Pescaderia',
         forks: '5',
         img: 'img/pescado.jpg',
+        phone: '123456789',
+        price: '€€€€€',
         foods: ['Salmón', 'Cap Roig', 'Cabiar', 'Dorada', 'Bacalao'],
         services: ['baños', 'Salón para descansar'],
         category: 'hostelry',
@@ -59,12 +79,21 @@ db.restaurants.insertMany([{
             comment: 'El gourmet del pescado está aquí',
             stars: 5,
             date: Date()
-        }]
+        },
+        {
+            author: 'Paco Tomás',
+            comment: 'La comida es buena, pero el servicio puede mejorar...',
+            stars: 2,
+            date: 'Fri Feb 05 2021 23:30:26 GMT+0000 (UTC)'
+        }
+    ]
     },
     {
         name: 'El Paraíso de los Carnívoros',
         forks: '5',
         img: 'img/carne.jpg',
+        phone: '987654321',
+        price: '€€€€',
         foods: ['Ternera', 'Carne de vaca', 'Vacuno', 'Pollo'],
         services: ['baños'],
         category: 'hostelry',
@@ -80,12 +109,21 @@ db.restaurants.insertMany([{
             comment: 'Si te gusta la carne, es el mejor restaurante de carne de la ciudad',
             stars: 5,
             date: Date()
-        }]
+        },
+        {
+            author: 'Jorge Ponce',
+            comment: 'Es decente....',
+            stars: 3,
+            date: Date()
+        }
+    ]
     },
     {
         name: 'Desayunos Saludables',
         forks: '3',
         img: 'img/saludables.jpg',
+        phone: '111111111',
+        price: '€€',
         foods: ['Tostadas', 'Cafés', 'Batidos', 'Infusiones'],
         services: ['baños'],
         category: 'hostelry',
@@ -101,6 +139,13 @@ db.restaurants.insertMany([{
             comment: 'El mejor restaurante para desayuna, con buenas vistas',
             stars: 3,
             date: Date()
-        }]
+        },
+        {
+            author: 'Jaume Lopez',
+            comment: 'Sitio agradable y familiar',
+            stars: 4,
+            date: Date()
+        }
+    ]
     }
 ])
