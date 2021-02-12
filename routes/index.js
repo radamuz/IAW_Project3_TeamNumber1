d8f6f3e6bb183b1002bc39477a8a21ef43851b70
Raúl Adamuz <radamuz16@gmail.com>
2021-02-12 17:22:58 +0100

    deleting log restaurants

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,6 @@ const { ensureAuth, ensureGuest } = require('../middelware/auth')
 router.get('/', async (req, res) => {
     try {
         const restaurants = await Restaurant.find().sort({forks:-1}).limit(5).lean()
-        console.log(restaurants)
 
         const avgComments = await Restaurant.aggregate([
             { 
