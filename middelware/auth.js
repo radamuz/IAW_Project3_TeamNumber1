41a3a9acf56806c4c39072d84fe8a1590714b784
Raúl Adamuz <radamuz16@gmail.com>
2021-02-12 11:52:27 +0100

    config variable and auth.js

--- middelware/auth.js
+++ middelware/auth.js
@@ -17,7 +17,7 @@ module.exports = {
     },
     ensureGuest: function (req, res, next) {
         if(req.isAuthenticated()){
-            res.redirect('/home')
+            res.redirect('/')
         }else{
             return next()
         }
